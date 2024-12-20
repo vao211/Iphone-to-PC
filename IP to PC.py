6b701f92ee722ce627e6161afb010be5f584fe84
vao211 <vaolu211@gmail.com>
2024-12-20 07:43:11 +0700

    remove .AAE file

--- IP to PC.py
+++ IP to PC.py
@@ -26,6 +26,7 @@ def remove_folders_keep_files(directory):
                 if os.path.isfile(sub_item_path):
                     destination_path = os.path.join(directory, sub_item)
 
+
                     if os.path.exists(destination_path):
                         print(f"Tệp đã tồn tại: {destination_path}. Đang thêm số đếm vào tên tệp.")
                         base, extension = os.path.splitext(sub_item)
@@ -35,9 +36,18 @@ def remove_folders_keep_files(directory):
                             count += 1
                     shutil.copy2(sub_item_path, destination_path)
                     print(f"Đã sao chép tệp: {sub_item_path} vào {destination_path}")
+
+            # Xóa folder
             shutil.rmtree(item_path)
             print(f"Đã xóa thư mục: {item_path}")
-            
+
+    # Xóa .AAE
+    for item in os.listdir(directory):
+        if item.endswith(".AAE"):
+            file_path = os.path.join(directory, item)
+            os.remove(file_path)
+            print(f"Đã xóa tệp: {file_path}")
+
 if __name__ == "__main__":
     print("Made by Ngài Vinh")
     command = "python \"D:\Code\copyImage_Iphone\" \"This PC\Apple iPhone\Internal Storage\DCIM\" \"C:\Iphone_copy\" "
