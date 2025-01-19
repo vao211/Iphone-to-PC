45c1fb9be8d312f81ec2e13d6efb486ac7335ea2
Vinhdeptraiso1 <vaolu211@gmail.com>
2025-01-19 21:10:24 +0700

    fix directory error

--- IP to PC.py
+++ IP to PC.py
@@ -50,12 +50,15 @@ def remove_folders_keep_files(directory):
 
 if __name__ == "__main__":
     print("Made by Ngài Vinh")
-    command = "python \"D:\Code\copyImage_Iphone\" \"This PC\Apple iPhone\Internal Storage\DCIM\" \"C:\Iphone_copy\" "
-    directory = "C:\Iphone_copy"
+    #đường dẫn
+    current_directory = os.path.dirname(os.path.abspath(__file__)) #Vị trí hiện tại
+    print("dir: ",current_directory)
+    command = f"python \"{current_directory}\" \"This PC\\Apple iPhone\\Internal Storage\\DCIM\" \"C:\\Iphone_copy\" "
+    directory = "C:\\Iphone_copy"
     run_powershell_command(command)
     remove_folders_keep_files(directory)
     
     messagebox.showinfo("Ngài Vinh thông báo", "Ảnh và video được lưu ở thư mục Iphone_copy trong ổ C"
-                        "\n\n C:\Iphone_copy "
+                        "\n\n C:\\Iphone_copy "
                         "\n\n Bấm ok để mở")
     subprocess.Popen(f'explorer "{directory}"', shell=True)
